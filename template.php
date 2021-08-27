917eb4c35235dfe4a28f11ac9d22ebef349bba70
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-08-27 18:00:00 -0400

    Add files via upload

---
+++ template.php
@@ -0,0 +1,25 @@
+<?php
+	require 'config/config.php';
+
+?>
+
+<html lang="en-us">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+
+<body>
+	<header>
+	
+	</header>
+	
+	<main>
+	
+	</main>
+	
+	<footer>
+	
+	</footer>
+</body>
+</html>
