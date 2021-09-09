ca8046252014d2207e74dbd89ab632f6bc3d95ad
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-09-08 21:41:21 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,16 @@
 <?php
 	#Configuration file for Stockify database 
 
-	# Major functions:
-	# Connect to database, return error if unable to
-	# Timezone and constructor variable creation
-	# Session and output buffer start
-
 	# Turn on output buffer for better loading
 	ob_start();
 
-	# Start service
+	# Start session
 	session_start();
 
 	# Set timezone (EST)
 	$timezone = date_default_timezone_set("America/New_York");
 
-	# Initialize $connect constructor variable 
+	# Initialize $connect variable for database connection
 	$connect = mysqli_connect("localhost", "root", "", "stockifydb");
 
 	# Error if it can not connect
