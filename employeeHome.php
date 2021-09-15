d287500057b554d17adf6d6526a320e1ec27bda4
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-09-15 00:35:46 -0400

    Add files via upload

---
+++ employeeHome.php
@@ -0,0 +1,27 @@
+<?php
+  require 'config/config.php';
+
+?>
+
+<html lang="en-us">
+<head>
+  <meta charset="UTF-8">
+  <title>Employee Home</title>
+  
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <header>
+    <h1>Employee Home</h1>
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+</html>
