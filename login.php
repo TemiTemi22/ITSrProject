614bfb8b40aa53ff8252f1ad1a2e83bb0a48d4fc
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-09-15 00:33:50 -0400

    Update login.php

--- login.php
+++ login.php
@@ -2,14 +2,15 @@
   #configuration file used to connect to the database
   require 'config/config.php';
 
-  #array to hold login errors so they can be displayed where we want them to
-  $errors = array();
+  #arrays to hold login errors so they can be displayed where we want them to errors1 is for manager errors and errors 2 is for employee errors
+  $errors1 = array();
+  $errors2 = array();
 
   #if the manager login button is pressed a query is ran to see if the credentials are in the database
   if(isset($_POST['managerLogin'])) {
     #username and password entered by the user
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    $username = mysqli_real_escape_string($connect, htmlspecialchars($_POST['username']));
+    $password = hash("sha3-512", mysqli_real_escape_string($connect, htmlspecialchars($_POST['password'])));
     
     #the query to the database that will return 1 if the user exists and 0 if they do not, mysqli_num_rows() is used to see how many results are renturned
     $query = mysqli_query($connect, "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND role = 'manager'");
@@ -22,7 +23,28 @@
     
     #else add an error statement to the error array to be displayed later
     else {
-      array_push($errors, "Username or Password is incorrect<br>");
+      array_push($errors1, "Username or Password is incorrect<br>");
+    }
+  }
+
+  #if the employee login button is pressed a query is ran to see if the credentials are in the database
+  if(isset($_POST['employeeLogin'])) {
+    #username and password entered by the user
+    $username = mysqli_real_escape_string($connect, htmlspecialchars($_POST['username']));
+    $password = hash("sha3-512", mysqli_real_escape_string($connect, htmlspecialchars($_POST['password'])));
+    
+    #the query to the database that will return 1 if the user exists and 0 if they do not, mysqli_num_rows() is used to see how many results are renturned
+    $query = mysqli_query($connect, "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND role = 'employee'");
+    $checkQuery = mysqli_num_rows($query);
+    
+    #if a result is returned redirect to the manager home page
+    if($checkQuery == 1) {
+      header("Location: employeeHome.php");
+    }
+    
+    #else add an error statement to the error array to be displayed later
+    else {
+      array_push($errors2, "Username or Password is incorrect<br>");
     }
   }
 ?>
@@ -31,17 +53,20 @@
 <head>
   <meta charset="UTF-8">
   <title>Stockify Login</title>
+  
+  <link rel="stylesheet" type="text/css" href="css/loginStyle.css">
 </head>
 
 <body>
   <header>
-	<center><h1>Stockify</h1></center>
+	<center><h1 id="logo">Stockify</h1></center>
+    <center><h2 id="logo2">An Inventory Control System</h2></center>
   </header>
 	
   <main>
     <!-- div containing the manager login form-->
-    <div class="login">
-      <h2>Manager Login</h2>
+    <div class="login" id="mlogin">
+      <center><h2 class="lheader">Manager Login</h2>
       
       <form action="login.php" method="post">
         <input type="text" name="username" placeholder="Username" required>
@@ -58,11 +83,37 @@
         
         <?php 
           #if the error exists in the error array display it
-          if(in_array("Username or Password is incorrect<br>", $errors)) {
-            echo "Username or Password is incorrect<br>";
+          if(in_array("Username or Password is incorrect<br>", $errors1)) {
+            echo "<p style='color: white'>Username or Password is incorrect</p><br>";
           }
         ?>
-      </form>
+        </form></center>
+    </div>
+    
+    <!-- div containing the employee login form-->
+    <div class="login" id="elogin">
+      <center><h2 class="lheader">Employee Login</h2>
+      
+      <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        
+        <br>
+        
+        <input type="password" name="password" placeholder="Password" required>
+        
+        <br>
+        
+        <input type="submit" name="employeeLogin" value="Employee Login">
+        
+        <br>
+        
+        <?php 
+          #if the error exists in the error array display it
+          if(in_array("Username or Password is incorrect<br>", $errors2)) {
+            echo "<p style='color: white'>Username or Password is incorrect</p><br>";
+          }
+        ?>
+      </form></center>
     </div>
     
   </main>
