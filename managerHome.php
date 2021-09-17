63b9e23351a46ce4cee46ecc74051b561c493715
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-09-17 01:19:26 -0400

    Update managerHome.php

--- managerHome.php
+++ managerHome.php
@@ -1,17 +1,23 @@
 <?php
   require 'config/config.php';
-
 ?>
 
 <html lang="en-us">
 <head>
   <meta charset="UTF-8">
   <title>Manager Home</title>
+  
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
   <header>
-    <h1>Manager Home</h1>
+    <a href="managerHome.php"><h1 id="headerlogo">Stockify</h1></a>
+    
+    <nav>
+      <a href="managerHome.php"><b>Home</b></a>
+      <a href="login.php"><b>Logout</b></a>
+    </nav>
   </header>
 
   <main>
