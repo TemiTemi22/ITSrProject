77c50f7a8f0ef589a44ae17cac630f4e84ee1d94
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-09-22 00:06:19 -0400

    Update managerHome.php

--- managerHome.php
+++ managerHome.php
@@ -1,5 +1,21 @@
 <?php
   require 'config/config.php';
+
+  $inventory = "";
+
+  $inventoryQuery = mysqli_query($connect, "SELECT * FROM products");
+
+  if(mysqli_num_rows($inventoryQuery) > 0){
+    while($results = mysqli_fetch_array($inventoryQuery)){
+      $productName = $results['product_name'];
+      $quantity = $results['quantity'];
+      
+      $inventory .= "<tr>
+                      <td>$productName</td>
+                      <td>$quantity</td>
+                     </tr>";
+    }
+  }
 ?>
 
 <html lang="en-us">
@@ -12,16 +28,41 @@
 
 <body>
   <header>
-    <a href="managerHome.php"><h1 id="headerlogo">Stockify</h1></a>
+    <div class="headerdiv">
+      <a href="managerHome.php"><h1 id="headerlogo">Stockify</h1></a>
     
-    <nav>
-      <a href="managerHome.php"><b>Home</b></a>
-      <a href="login.php"><b>Logout</b></a>
-    </nav>
+      <nav class="headerlinks">
+        <a href="managerHome.php"><b>Home</b></a>
+        <a href="login.php"><b>Logout</b></a>
+      </nav>
+    </div>
   </header>
 
   <main>
-
+    <div class="pagelinksdiv">
+      <nav class="pagelinksnav">
+        <center>
+          <a href=""><b>Product Analytics</b></a>
+          <a href=""><b>Create Product</b></a>
+          <a href=""><b>Update Product</b></a>
+          <a href=""><b>Delete Product</b></a>
+          <a href=""><b>Send Announcement</b></a>
+        </center>
+      </nav>
+    </div>
+    
+    <div class="inventorydiv">
+      <center>
+        <table class="inventory">
+          <tr>
+            <th><b>Product Name</b></th>
+            <th><b>Quantity</b></th>
+          </tr>
+          
+          <?php echo $inventory; ?>
+        </table>
+      </center>
+    </div>
   </main>
 
   <footer>
