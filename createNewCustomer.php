57fe538482d04305426470ae8e48e502ebc81a5a
Jabuba14 <89614041+Jabuba14@users.noreply.github.com>
2021-10-12 23:50:18 -0400

    Add files via upload

---
+++ createNewCustomer.php
@@ -0,0 +1,72 @@
+<?php
+require 'config/config.php';
+
+  $notifications = array();
+
+  if(!isset($_SESSION['username'])){
+    header('Location: login.php');
+  }
+
+  if(isset($_POST['createNewCustomer'])){
+    $customerName = mysqli_real_escape_string($connect, htmlspecialchars($_POST['customerName']));
+    $customerPhoneNumber = mysqli_real_escape_string($connect, htmlspecialchars($_POST['customerPhoneNumber']));
+    $customerAddress = mysqli_real_escape_string($connect, htmlspecialchars($_POST['customerAddress']));
+    
+    $query = mysqli_query($connect, "INSERT INTO customers VALUES ('', '$customerName', '$customerPhoneNumber')");
+    $query2 = mysqli_query($connect, "INSERT INTO customer_address VALUES ('', '$customerName', '$customerAddress')");
+    
+    array_push($notifications, "Customer Created");
+  }
+?>
+
+<html lang="en-us">
+<head>
+	<meta charset="UTF-8">
+	<title>Create New Customer</title>
+  
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<header>
+      <div class="headerdiv">
+        <a href="employeeHome.php"><h1 id="headerlogo">Stockify</h1></a>
+
+        <nav class="headerlinks">
+          <a href="employeeHome.php"><b>Home</b></a>
+          <a href="logout.php"><b>Logout</b></a>
+        </nav>
+      </div>
+	</header>
+	
+	<main>
+	   <div class="container">
+        <center>
+          <h1 class="title">Create New Customer</h1>
+          
+          <form action="createNewCustomer.php" method="post">
+            <input type="text" name="customerName" placeholder="Name" required>
+            <input type="tel" name="customerPhoneNumber" placeholder="Phone Number: 123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+            <input type="text" name="customerAddress" placeholder="Address" required>
+            
+            <br>
+            
+            <input type="submit" name="createNewCustomer" value="Create New Customer">
+        
+            <br>
+            
+            <?php 
+              if(in_array("Customer Created", $notifications)) {
+                echo "<p style='color: white'>Customer Created</p>";
+              }
+            ?>
+          </form>
+        </center>
+      </div>
+	</main>
+	
+	<footer>
+	
+	</footer>
+</body>
+</html>
---
+++ createProduct.php
@@ -0,0 +1,103 @@
+<?php
+require 'config/config.php';
+
+  $notifications = array();
+
+  if(!isset($_SESSION['username'])){
+    header('Location: login.php');
+  }
+
+  if(isset($_POST['createProduct'])){
+    $productName = mysqli_real_escape_string($connect, htmlspecialchars($_POST['productName']));
+    $quantity = mysqli_real_escape_string($connect, htmlspecialchars($_POST['quantity']));
+    $bufferStock = mysqli_real_escape_string($connect, htmlspecialchars($_POST['bufferStock']));
+    $leadTime = mysqli_real_escape_string($connect, htmlspecialchars($_POST['leadTime']));
+    $attributes = mysqli_real_escape_string($connect, htmlspecialchars($_POST['attributes']));
+    $dateAdded = new DateTime("now");
+    $dateString = $dateAdded->format('Y-m-d H:i:s');
+    
+    $query = mysqli_query($connect, "SELECT * FROM products WHERE product_name = '$productName'");
+    $num_results = mysqli_num_rows($query);
+    
+    if($num_results == 1){
+      array_push($notifications, "Product already exists");
+    }
+    
+    elseif($num_results == 0){
+      if($leadTime > 5){
+        array_push($notifications, "Lead time must be a value between 1 and 5");
+      }
+      
+      else{
+        $query2 = mysqli_query($connect, "INSERT INTO products VALUES ('', '$productName', '$quantity', '$bufferStock', '$leadTime', '', '$dateString', '$attributes')");
+        
+        array_push($notifications, "Product Created");
+      }
+    }
+  }
+?>
+
+<html lang="en-us">
+<head>
+	<meta charset="UTF-8">
+	<title>Create Product</title>
+  
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<header>
+      <div class="headerdiv">
+        <a href="managerHome.php"><h1 id="headerlogo">Stockify</h1></a>
+
+        <nav class="headerlinks">
+          <a href="managerHome.php"><b>Home</b></a>
+          <a href="logout.php"><b>Logout</b></a>
+        </nav>
+      </div>
+	</header>
+	
+	<main>
+	   <div class="container">
+        <center>
+          <h1 class="title">Create Product</h1>
+          
+          <form action="createProduct.php" method="post">
+            <input type="text" name="productName" placeholder="Product Name" required>
+            <input type="number" name="quantity" placeholder="Quantity" required>
+            <input type="number" name="bufferStock" placeholder="Buffer Stock" required>
+            
+            <br>
+            
+            <input type="number" name="leadTime" placeholder="Lead Time: Value 1-5" required>
+            <input type="text" name="attributes" placeholder="Attributes" required>
+            
+            <br>
+            
+            <input type="submit" name="createProduct" value="Create Product">
+        
+            <br>
+            
+            <?php 
+              if(in_array("Product already exists", $notifications)) {
+                echo "<p style='color: white'>Product already exists</p>";
+              }
+            
+              if(in_array("Lead time must be a value between 1 and 5", $notifications)) {
+                echo "<p style='color: white'>Lead time must be a value between 1 and 5</p>";
+              }
+            
+              if(in_array("Product Created", $notifications)) {
+                echo "<p style='color: white'>Product Created</p>";
+              }
+            ?>
+          </form>
+        </center>
+      </div>
+	</main>
+	
+	<footer>
+	
+	</footer>
+</body>
+</html>
